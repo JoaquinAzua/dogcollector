644ebddb57e084e6a448211338d25f19e7ba0bb5
Joaquin Azua <joaquin.diego.azua@gmail.com>
2022-01-14 00:32:24 -0600

    Toy model full functionality

--- main_app/urls.py
+++ main_app/urls.py
@@ -21,6 +21,19 @@ urlpatterns = [
     path('dogs/<int:pk>/delete/', views.DogDelete.as_view(), name='dogs_delete'),
 
     # 'dogs/<int:cat_id>/add_feeding/', - Add Feeding Route
-    path('dogs/<int:dog_id>/add_feeding/', views.add_feeding, name='add_feeding')
+    path('dogs/<int:dog_id>/add_feeding/', views.add_feeding, name='add_feeding'),
+
+    path('dogs/<int:dog_id>/assoc_toy/<int:toy_id>/', views.assoc_toy, name='assoc_toy'),
+
+    path('toys/', views.ToyList.as_view(), name='toys_index'),
+
+    path('toys/<int:pk>/', views.ToyDetail.as_view(), name='toys_detail'),
+    
+    path('toys/create/', views.ToyCreate.as_view(), name='toys_create'),
+
+    path('toys/<int:pk>/update/', views.ToyUpdate.as_view(), name='toys_update'),
+
+    path('toys/<int:pk>/delete/', views.ToyDelete.as_view(), name='toys_delete'),
+
 
 ]
